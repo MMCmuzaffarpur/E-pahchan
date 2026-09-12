0754e9d8a077103beb4cc8ff47b68586554637bd
MMCmuzaffarpur <mmcmuzaffarpur@gmail.com>
2026-09-12 16:24:30 +0530

    Update esicLogoData.ts

--- src/utils/esicLogoData.ts
+++ src/utils/esicLogoData.ts
@@ -1,4 +1,3 @@
-// Root assets folder se logo.png ko Vite ke zariye import karein
-import localEsicLogo from '../../../assets/logo.png';
+import localEsicLogo from '../../assets/logo.png';
 
 export const ESIC_EMBEDDED_LOGO = localEsicLogo;
